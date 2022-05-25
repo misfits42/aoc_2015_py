a4cde45bc97e1f493c8b91cd82e9faa433802e62
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-05-25 13:51:48 +1000

    Solve Day 1 (both parts)

---
+++ solutions/day_01.py
@@ -0,0 +1,37 @@
+import time
+
+def main():
+    # Read in problem input
+    with open("./inputs/day_01.txt") as file:
+        input = file.read().strip()
+        p1_solution = solve_part1(input)
+        p2_solution = solve_part2(input)
+        print("P1 solution - {}".format(p1_solution))
+        print("P2 solution - {}".format(p2_solution))
+
+def solve_part1(input) -> int:
+    floor = 0
+    for c in input:
+        if c == '(':
+            floor += 1
+        else:
+            floor -= 1
+    return floor
+
+def solve_part2(input) -> int:
+    pos = 1
+    floor = 0
+    for c in input:
+        if c == '(':
+            floor += 1
+        else:
+            floor -= 1
+    
+        if floor == -1:
+            break
+        else:
+            pos += 1
+    return pos
+
+if __name__ == "__main__":
+    main()
