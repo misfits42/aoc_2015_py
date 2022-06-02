c1a1d0fb863fa006800379f08dfc40decf6fda63
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-02 22:28:25 +1000

    Solve Day 14 Part 2

--- solutions/day_14.py
+++ solutions/day_14.py
@@ -1,6 +1,39 @@
 import re
 
 
+class Reindeer:
+    def __init__(self, name, speed, travel_duration, rest_duration):
+        """
+        Creates a new Reindeer that is within travel phase, without having
+        travelled any distance yet.
+        """
+        self.name = name
+        self.speed = speed
+        self.travel_duration = travel_duration
+        self.rest_duration = rest_duration
+        self.is_travelling = True
+        self.seconds_travelled = 0
+        self.seconds_rested = 0
+        self.distance_travelled = 0
+
+    def advance_one_second(self):
+        """
+        Advances the Reindeer forward by one second in time, increasing distance
+        travelled and switching between travel/rest phases as required.
+        """
+        if self.is_travelling:
+            self.seconds_travelled += 1
+            self.distance_travelled += self.speed   # Implicitly mult by 1 second
+            if self.seconds_travelled == self.travel_duration:
+                self.seconds_travelled = 0
+                self.is_travelling = False      # Reindeer enters rest phase
+        else:   # reindeer is resting
+            self.seconds_rested += 1
+            if self.seconds_rested == self.rest_duration:
+                self.seconds_rested = 0
+                self.is_travelling = True       # Reindeer enters travel phase
+
+
 def main():
     input = process_input_file()
     p1_solution = solve_part1(input)
@@ -29,16 +62,19 @@ def process_input_file():
 
 
 def solve_part1(input):
+    """
+    Calculates the total distance travelled by the winning reindeer.
+    """
     race_duration = 2503    # seconds
     distances_traveled = {}
-    for reindeer_name in input.keys():
-        params = input[reindeer_name]
+    for (reindeer_name, params) in input.items():
         # Calculate how many full travel periods are completed by reindeer, and spare seconds
         period_dur = params[1] + params[2]
         complete_periods_travels = race_duration // period_dur  # integer division required
         spare_seconds = race_duration % period_dur
         # Calculate total distance travelled
-        distances_traveled[reindeer_name] = complete_periods_travels * params[0] * params[1]
+        distances_traveled[reindeer_name] = complete_periods_travels * \
+            params[0] * params[1]
         if spare_seconds <= params[1]:
             distances_traveled[reindeer_name] += params[0] * spare_seconds
         else:
@@ -47,7 +83,36 @@ def solve_part1(input):
 
 
 def solve_part2(input):
-    ()
+    """
+    Calculates the total points awarded to the winning reindeer.
+    """
+    race_duration = 2503    # seconds
+    # Create the reindeers
+    reindeers = []
+    points = {}
+    for (reindeer_name, params) in input.items():
+        reindeers.append(
+            Reindeer(reindeer_name, params[0], params[1], params[2]))
+        points[reindeer_name] = 0
+    # Conduct reindeer race, awarding one point to lead reindeer after each second
+    for _x in range(0, race_duration):
+        running_distances = {}
+        for i in range(0, len(reindeers)):
+            reindeers[i].advance_one_second()
+            running_distances[reindeers[i].name] = reindeers[i].distance_travelled
+        # Find lead reindeers and award point/s
+        lead_reindeers = []
+        lead_distance = None
+        for (reindeer_name, distance) in running_distances.items():
+            distance = running_distances[reindeer_name]
+            if lead_distance is None or distance > lead_distance:
+                lead_distance = distance
+                lead_reindeers = [reindeer_name]
+            elif distance == lead_distance:
+                lead_reindeers.append(reindeer_name)
+        for reindeer_name in lead_reindeers:
+            points[reindeer_name] += 1
+    return max(points.values())
 
 
 if __name__ == "__main__":
--- tests/solutions_test_methods.py
+++ tests/solutions_test_methods.py
@@ -124,12 +124,12 @@ class SolutionsTestMethods(unittest.TestCase):
         input = solutions.day_12.process_input_file()
         solution = solutions.day_12.solve_part2(input)
         self.assertEqual(96852, solution)
-    
+
     def test_day_13_p1(self):
         input = solutions.day_13.process_input_file()
         solution = solutions.day_13.solve_part1(input)
         self.assertEqual(664, solution)
-    
+
     def test_day_13_p2(self):
         input = solutions.day_13.process_input_file()
         solution = solutions.day_13.solve_part2(input)
@@ -139,6 +139,11 @@ class SolutionsTestMethods(unittest.TestCase):
         input = solutions.day_14.process_input_file()
         solution = solutions.day_14.solve_part1(input)
         self.assertEqual(2640, solution)
+
+    def test_day_14_p2(self):
+        input = solutions.day_14.process_input_file()
+        solution = solutions.day_14.solve_part2(input)
+        self.assertEqual(1102, solution)
 
 
 if __name__ == "__main__":
