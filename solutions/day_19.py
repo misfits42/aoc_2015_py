ccbb22b16c12dbc22f6fb636893b8d631f28f180
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-05 17:25:25 +1000

    Remove unused Day 19 code and add comments

--- solutions/day_19.py
+++ solutions/day_19.py
@@ -20,19 +20,23 @@ def process_input_file():
             if len(line) == 0:
                 recipes_phase = not recipes_phase
                 continue
-            if recipes_phase:
+            if recipes_phase:   # Extract replacement recipes
                 m = regex_recipe.match(line)
                 find = m.group(1)
                 replace = m.group(2)
                 if find not in replacement_recipes:
                     replacement_recipes[find] = []
                 replacement_recipes[find].append(replace)
-            else:
+            else:   # Extract input molecule
                 input_molecule = line
     return (input_molecule, replacement_recipes)
 
 
 def solve_part1(input):
+    """
+    Determines the number of unique molecules that can be created by conducting
+    all possible replacements on the input molecule.
+    """
     unique_outputs = set()
     input_molecule = input[0]
     replacement_recipes = input[1]
@@ -42,7 +46,8 @@ def solve_part1(input):
             replaced = 0
             for i in range(0, len(input_molecule)):
                 if input_molecule[i:(i + len(find))] == find:
-                    output = input_molecule[:i] + replace + input_molecule[i + len(find):]
+                    output = input_molecule[:i] + replace + \
+                        input_molecule[i + len(find):]
                     unique_outputs.add(output)
                     # Stop if no further replacements possible
                     replaced += 1
@@ -53,21 +58,6 @@ def solve_part1(input):
 
 def solve_part2(input):
     ()
-
-
-def replace_nth_occurrence(input_str, find, replace, n):
-    f_index = input_str.find(find)
-    i = 1
-    while True:
-        if f_index == -1:
-            break
-        if i == n:
-            output = input_str[:f_index] + replace + input_str[f_index + len(find):]
-            return output
-        else:
-            f_index = input_str.find(find, f_index + len(find) + 1)
-            i += 1
-    return input_str
 
 
 if __name__ == "__main__":
