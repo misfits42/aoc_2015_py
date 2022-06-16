429456d96e3bee1c593922eea28f428bde76eb16
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-16 21:23:51 +1000

    NOT WORKING - result is too low

--- solutions/day_22.py
+++ solutions/day_22.py
@@ -13,32 +13,32 @@ class Spell(Enum):
     def get_mana_cost(self):
         match self:
             case Spell.MAGIC_MISSILE:
-                53
+                return 53
             case Spell.DRAIN:
-                73
+                return 73
             case Spell.SHIELD:
-                113
+                return 113
             case Spell.POISON:
-                173
+                return 173
             case Spell.RECHARGE:
-                229
+                return 229
 
     def get_effect_turns(self):
         match self:
             case Spell.MAGIC_MISSILE:
-                0
+                return 0
             case Spell.DRAIN:
-                0
+                return 0
             case Spell.SHIELD:
-                6
+                return 6
             case Spell.POISON:
-                6
+                return 6
             case Spell.RECHARGE:
-                5
+                return 5
 
     @staticmethod
     def list():
-        return list(map(lambda c: c.value, Spell))
+        return list(map(lambda c: c, Spell))
 
 
 class Entity:
@@ -68,9 +68,12 @@ class Player(Entity):
             damage_to_deal = 1
         self.hp -= damage_to_deal
 
+    def can_cast_with_mana(self, spell):
+        return self.mana >= spell.get_mana_cost()
+
     def cast_spell(self, spell, enemy):
         # Check if player has enough mana remaining
-        if self.mana < spell.get_mana_cost() or spell in self.effects:
+        if self.mana < spell.get_mana_cost() or spell in self.effects.keys():
             return False
         # Expend mana to cast spell
         self.total_mana_spent += spell.get_mana_cost()
@@ -82,29 +85,30 @@ class Player(Entity):
                 enemy.deal_damage(4)
                 self.heal(2)
             case Spell.SHIELD:
-                self.effects[spell, spell.get_effect_turns()]
+                self.effects[spell] = spell.get_effect_turns()
                 self.armour += 7
             case Spell.POISON:
-                self.effects[spell, spell.get_effect_turns()]
+                self.effects[spell] = spell.get_effect_turns()
             case Spell.RECHARGE:
-                self.effect[spell, spell.get_effect_turns()]
+                self.effects[spell] = spell.get_effect_turns()
+        return True
 
     def process_effects(self, enemy):
         effects_to_remove = []
-        for (effect, turns_remaining) in self.effects:
-            turns_remaining -= 1
+        for (effect) in self.effects.keys():
+            self.effects[effect] -= 1
             match effect:
                 case Spell.SHIELD:
-                    if turns_remaining == 0:
+                    if self.effects[effect] == 0:
                         self.armour -= 7
                         effects_to_remove.append(effect)
                 case Spell.POISON:
                     enemy.deal_damage(3)
-                    if turns_remaining == 0:
+                    if self.effects[effect] == 0:
                         effects_to_remove.append(effect)
                 case Spell.RECHARGE:
                     self.mana += 101
-                    if turns_remaining == 0:
+                    if self.effects[effect] == 0:
                         effects_to_remove.append(effect)
         for effect in effects_to_remove:
             self.effects.pop(effect)
@@ -145,11 +149,48 @@ def solve_part2(input):
 
 
 def conduct_fight(player, boss):
-    ()
+    mana_observed = []
+    conduct_fight_recursive(player, boss, mana_observed)
+    return min(mana_observed)
 
 
-def conduct_fight_recursive(player, boss, mana_expended):
-    ()
+def conduct_fight_recursive(player, boss, mana_observed):
+    if len(mana_observed) > 0:
+        print(min(mana_observed))
+    # Select player spell
+    for spell in Spell.list():
+        # Copy player and boss
+        new_player = deepcopy(player)
+        new_boss = deepcopy(boss)
+        # Player turn
+        # - Process player effects and check if boss is dead
+        new_player.process_effects(new_boss)
+        if new_boss.is_dead():
+            mana_observed.append(new_player.mana)
+            return
+        # - Cast spell
+        mana_check = new_player.can_cast_with_mana(spell)
+        if not mana_check:
+            return
+        _good_cast = new_player.cast_spell(spell, new_boss)
+        # - Check if boss is dead
+        if new_boss.is_dead():
+            mana_observed.append(new_player.mana)
+            return
+
+        # Boss turn
+        # - Process player effects and check if boss is dead
+        new_player.process_effects(new_boss)
+        if new_boss.is_dead():
+            mana_observed.append(new_player.mana)
+            return
+        # - Boss deal damage to player
+        new_player.deal_damage(new_boss.damage)
+        # - Check if player is dead
+        if new_player.is_dead():
+            return
+        # Player and boss still alive, so go to next turn
+        conduct_fight_recursive(new_player, new_boss, mana_observed)
 
 
 if __name__ == "__main__":
