e48efe14119f81f2aeda5632e3ec63e4a2108960
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-19 09:35:16 +1000

    Update Day 25 to match pylint style

--- solutions/day_25.py
+++ solutions/day_25.py
@@ -1,27 +1,38 @@
+"""
+Solutions for AOC 2015 Day 25.
+"""
+
+
 import re
 
 
 def main():
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    """
+    Solves AOC 2015 Day 25 Parts 1 and 2, printing out the solutions.
+    """
+    input_p = process_input_file()
+    p1_solution = solve_part1(input_p)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_p)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
+    """
+    Processes the AOC 2015 Day 25 input file into format for solver functions.
+    """
     regex_grid = re.compile(r"row (\d+), column (\d+)")
-    with open("./inputs/day_25.txt") as file:
+    with open("./inputs/day_25.txt", encoding="utf-8") as file:
         raw_input = file.read().strip()
-        m = regex_grid.search(raw_input)
-        return (int(m.group(1)), int(m.group(2)))
+        r_match = regex_grid.search(raw_input)
+        return (int(r_match.group(1)), int(r_match.group(2)))
 
 
-def solve_part1(input):
+def solve_part1(input_p):
     """
     Calculates the code required to start-up the weather machine.
     """
-    (row, column) = input
+    (row, column) = input_p
     # First stage
     seq = sum(range(1, column + 1))    # Initially the column top value
     increment = column
@@ -29,18 +40,18 @@ def solve_part1(input):
         seq += increment
         increment += 1
     # Second stage
-    code = 20151125 # Value in grid at r,c (1,1)
+    code = 20151125  # Value in grid at r,c (1,1)
     for _ in range(1, seq):
         code *= 252533
         code %= 33554393
     return code
 
 
-def solve_part2(input):
+def solve_part2(_input_p):
     """
     No solution required - Christmas has been saved!
     """
-    ()
+    return ()
 
 
 if __name__ == "__main__":
