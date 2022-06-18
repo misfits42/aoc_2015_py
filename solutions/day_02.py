9ac3d7662da48738ce479b310a6d9d07d29db758
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-19 09:47:56 +1000

    Update Day 2 code to match pylint style

--- solutions/day_02.py
+++ solutions/day_02.py
@@ -1,41 +1,61 @@
+"""
+Solutions for AOC 2015 Day 2.
+"""
+
+
 import re
 
 
 def main():
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    """
+    Solves AOC 2015 Day 2 Parts 1 and 2, printing out the solutions.
+    """
+    input_p = process_input_file()
+    p1_solution = solve_part1(input_p)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_p)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
-    with open("./inputs/day_02.txt") as file:
+    """
+    Processes the AOC 2015 Day 2 input file into format for solver functions.
+    """
+    with open("./inputs/day_02.txt", encoding="utf-8") as file:
         raw_input = file.read().strip()
         data = []
-        regex = re.compile("(\d+)x(\d+)x(\d+)")
+        regex = re.compile(r"(\d+)x(\d+)x(\d+)")
         for line in raw_input.splitlines():
             line = line.strip()
             if len(line) == 0:
                 continue
-            m = regex.match(line)
-            line_data = (int(m.group(1)), int(m.group(2)), int(m.group(3)))
+            match_r = regex.match(line)
+            line_data = (int(match_r.group(1)), int(match_r.group(2)),
+                         int(match_r.group(3)))
             data.append(line_data)
     return data
 
 
-def solve_part1(input) -> int:
-    total_paper = 0
-    for data in input:
+def solve_part1(input_p):
+    """
+    Calculates the total square feet of wrapping paper required to wrap the
+    presents specified in input.
+    """
+    total_paper = 0  # unit: square feet
+    for data in input_p:
         side_areas = [data[0] * data[1], data[0] * data[2], data[1] * data[2]]
         slack = min(side_areas)
         total_paper += 2 * sum(side_areas) + slack
     return total_paper
 
 
-def solve_part2(input) -> int:
-    total_ribbon = 0
-    for data in input:
+def solve_part2(input_p) -> int:
+    """
+    Calculates the total length of ribbon in feet required to wrap the presents
+    specified in input.
+    """
+    total_ribbon = 0    # unit: feet
+    for data in input_p:
         side_perims = [2 * (data[0] + data[1]), 2 *
                        (data[0] + data[2]), 2 * (data[1] + data[2])]
         volume = data[0] * data[1] * data[2]
