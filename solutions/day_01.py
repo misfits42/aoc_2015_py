23fc2cd476059645166c108cc37cc7c85a9aac65
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-19 09:41:55 +1000

    Update Day 1 code to match pyline style

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -1,33 +1,50 @@
+"""
+Solutions for AOC 2015 Day 1.
+"""
+
+
 def main():
+    """
+    Solves AOC 2015 Day 1 Parts 1 and 2, printing out the solutions.
+    """
     # Read in problem input
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    input_p = process_input_file()
+    p1_solution = solve_part1(input_p)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_p)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
-    with open("./inputs/day_01.txt") as file:
-        input = file.read().strip()
-    return input
+    """
+    Processes the AOC 2015 Day 1 input file into format for solvers.
+    """
+    with open("./inputs/day_01.txt", encoding="utf-8") as file:
+        return file.read().strip()
 
 
-def solve_part1(input) -> int:
+def solve_part1(input_p):
+    """
+    Calculates the final floor result from processing the input instructions.
+    """
     floor = 0
-    for c in input:
-        if c == '(':
+    for char in input_p:
+        if char == '(':
             floor += 1
         else:
             floor -= 1
     return floor
 
 
-def solve_part2(input) -> int:
+def solve_part2(input_p):
+    """
+    Calculates the location of the input character that causes Santa to first
+    enter the basement floor.
+    """
     pos = 1
     floor = 0
-    for c in input:
-        if c == '(':
+    for char in input_p:
+        if char == '(':
             floor += 1
         else:
             floor -= 1
