f6faa5fa4d948a6f64fb0c5c8cce350f65cbb97a
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-19 16:11:53 +1000

    Update Day 10 code to conform with pylint style

--- solutions/day_10.py
+++ solutions/day_10.py
@@ -1,54 +1,75 @@
-from unittest import result
+"""
+Solutions for AOC 2015 Day 10.
+"""
 
 
 def main():
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    """
+    Solves AOC 2015 Day 10 Parts 1 and 2, printing out the solutions.
+    """
+    input_p = process_input_file()
+    p1_solution = solve_part1(input_p)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_p)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
-    with open("./inputs/day_10.txt") as file:
+    """
+    Processes the AOC 2015 Day 10 input file into the format required by the
+    solver functions.
+    """
+    with open("./inputs/day_10.txt", encoding="utf-8") as file:
         return file.read().strip()
 
 
-def solve_part1(input):
-    result_str = str(input)
+def solve_part1(input_p):
+    """
+    Applies 40 iterations of the "look-and-say" process to the input string,
+    returning the resulting string.
+    """
+    result_string = str(input_p)
     for _ in range(0, 40):
-        result_str = apply_looksay_iteration(result_str)
-    return len(result_str)
+        result_string = apply_looksay_iteration(result_string)
+    return len(result_string)
 
 
-def solve_part2(input):
-    result_str = str(input)
+def solve_part2(input_p):
+    """
+    Applies 50 iterations of the "look-and-say" process to the input string,
+    returning the resulting string.
+    """
+    result_str = str(input_p)
     for _ in range(0, 50):
         result_str = apply_looksay_iteration(result_str)
     return len(result_str)
 
 
-def apply_looksay_iteration(input_str):
-    output_str = str("")
+def apply_looksay_iteration(input_string):
+    """
+    Returns the result of applying one iteration of the "look-and-say" process
+    to the input string.
+    """
+    output_string = str("")
     i = 0
     while True:
-        if i >= len(input_str):
+        if i >= len(input_string):
             break
         # Check current character
-        current_c = input_str[i]
+        char = input_string[i]
         streak_len = 1
         # Check if next char is same
         while True:
             i += 1
-            if i >= len(input_str):
+            if i >= len(input_string):
                 break
-            if input_str[i] == current_c:
+            if input_string[i] == char:
                 streak_len += 1
             else:
                 break
         # Add to the output string
-        output_str = "".join([output_str, str(streak_len), current_c])
-    return output_str
+        output_string = "".join([output_string, str(streak_len), char])
+    return output_string
 
 
 if __name__ == "__main__":
