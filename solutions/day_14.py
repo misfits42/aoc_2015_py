0b9ed92f63703fcff1ed34896f48fd543cd255a1
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-24 11:38:42 +1000

    Update Day 14 code to conform with pylint style

--- solutions/day_14.py
+++ solutions/day_14.py
@@ -1,7 +1,18 @@
+"""
+Solutions for AOC 2015 Day 14.
+"""
+
 import re
 
 
 class Reindeer:
+    """
+    Represents a single Reindeer involved in the race described in the AOC 2015
+    Day 14 problem. The reindeer starts by travelling at its designated speed
+    for its travel duration, then rests for its designated rest period until it
+    re-enters the travel phase phase.
+    """
+
     def __init__(self, name, speed, travel_duration, rest_duration):
         """
         Creates a new Reindeer that is within travel phase, without having
@@ -33,78 +44,101 @@ class Reindeer:
                 self.seconds_rested = 0
                 self.is_travelling = True       # Reindeer enters travel phase
 
+    def calculate_complete_cycles_and_spare_seconds(self, race_duration):
+        """
+        For the given race duration, calculates the total number of complete
+        travel periods the reindeer will complete and the number of spare
+        seconds for the final incomplete travel period. Return value is tuple
+        containing these two values in order.
+        """
+        reindeer_cycle_duration = self.travel_duration + self.rest_duration
+        complete_cycles = race_duration // reindeer_cycle_duration
+        spare_seconds = race_duration % reindeer_cycle_duration
+        return (complete_cycles, spare_seconds)
+
+    def calculate_distance_travelled_in_race(self, race_duration):
+        """
+        Calculates the total distance the Reindeer would travel during a race
+        for the specfied duration.
+        """
+        (complete_cycles, spare_seconds) = \
+            self.calculate_complete_cycles_and_spare_seconds(race_duration)
+        distance_travelled = complete_cycles * self.speed * self.travel_duration
+        if spare_seconds <= self.travel_duration:
+            distance_travelled += self.speed * spare_seconds
+        else:
+            distance_travelled += self.speed * self.travel_duration
+        return distance_travelled
+
 
 def main():
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    """
+    Solves AOC 2015 Day 14 Parts 1 and 2, printing out the solutions.
+    """
+    input_data = process_input_file()
+    p1_solution = solve_part1(input_data)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_data)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
-    input = {}
+    """
+    Processes the AOC 2015 Day 14 input file into the format required by the
+    solver functions.
+    """
+    input_data = []
     regex_reindeer = re.compile(
         r"^([A-Z][a-z]+) can fly (\d+) km/s for (\d+) seconds, but then must "
         r"rest for (\d+) seconds.$")
-    with open("./inputs/day_14.txt") as file:
+    with open("./inputs/day_14.txt", encoding="utf-8") as file:
         for line in file.readlines():
             line = line.strip()
             if len(line) == 0:
                 continue
-            m = regex_reindeer.match(line)
-            reindeer_name = m.group(1)
-            speed = int(m.group(2))             # km/s
-            travel_duration = int(m.group(3))   # seconds
-            rest_duration = int(m.group(4))     # seconds
-            input[reindeer_name] = (speed, travel_duration, rest_duration)
-    return input
+            match_reindeer = regex_reindeer.match(line)
+            name = match_reindeer.group(1)
+            speed = int(match_reindeer.group(2))             # km/s
+            travel_duration = int(match_reindeer.group(3))   # seconds
+            rest_duration = int(match_reindeer.group(4))     # seconds
+            reindeer = Reindeer(name, speed, travel_duration, rest_duration)
+            input_data.append(reindeer)
+    return input_data
 
 
-def solve_part1(input):
+def solve_part1(input_data):
     """
     Calculates the total distance travelled by the winning reindeer.
     """
     race_duration = 2503    # seconds
-    distances_traveled = {}
-    for (reindeer_name, params) in input.items():
-        # Calculate how many full travel periods are completed by reindeer, and spare seconds
-        period_dur = params[1] + params[2]
-        complete_periods_travels = race_duration // period_dur  # integer division required
-        spare_seconds = race_duration % period_dur
-        # Calculate total distance travelled
-        distances_traveled[reindeer_name] = complete_periods_travels * \
-            params[0] * params[1]
-        if spare_seconds <= params[1]:
-            distances_traveled[reindeer_name] += params[0] * spare_seconds
-        else:
-            distances_traveled[reindeer_name] += params[0] * params[1]
-    return max(distances_traveled.values())
+    distances_travelled = []
+    for reindeer in input_data:
+        distance = reindeer.calculate_distance_travelled_in_race(race_duration)
+        distances_travelled.append(distance)
+    return max(distances_travelled)
 
 
-def solve_part2(input):
+def solve_part2(input_data):
     """
     Calculates the total points awarded to the winning reindeer.
     """
     race_duration = 2503    # seconds
     # Create the reindeers
-    reindeers = []
+    reindeers = input_data
     points = {}
-    for (reindeer_name, params) in input.items():
-        reindeers.append(
-            Reindeer(reindeer_name, params[0], params[1], params[2]))
-        points[reindeer_name] = 0
+    for reindeer in input_data:
+        points[reindeer.name] = 0
     # Conduct reindeer race, awarding one point to lead reindeer after each second
-    for _x in range(0, race_duration):
+    for _ in range(0, race_duration):
+        # Advance each reindeer by one second and record their travelled dist
         running_distances = {}
-        for i in range(0, len(reindeers)):
-            reindeers[i].advance_one_second()
-            running_distances[reindeers[i].name] = reindeers[i].distance_travelled
-        # Find lead reindeers and award point/s
+        for reindeer in reindeers:
+            reindeer.advance_one_second()
+            running_distances[reindeer.name] = reindeer.distance_travelled
+        # Determine the lead reindeer/s and award them each one point
         lead_reindeers = []
         lead_distance = None
         for (reindeer_name, distance) in running_distances.items():
-            distance = running_distances[reindeer_name]
             if lead_distance is None or distance > lead_distance:
                 lead_distance = distance
                 lead_reindeers = [reindeer_name]
