e2efdb5799bfa6d9211d71529a214122e8e3d45d
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-24 11:10:43 +1000

    Update Day 13 code to conform with pylint style

--- solutions/day_13.py
+++ solutions/day_13.py
@@ -1,90 +1,109 @@
+"""
+Solutions for AOC 2015 Day 13.
+"""
+
 import itertools
 import re
 
 
 def main():
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    """
+    Solves AOC 2015 Day 13 Parts 1 and 2, printing out the solutions.
+    """
+    input_data = process_input_file()
+    p1_solution = solve_part1(input_data)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_data)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
-    input = {}
-    regex_happ = re.compile(
+    """
+    Processes the AOC 2015 Day 13 input file into the format required by the
+    solver functions. Returned data structure is a dictionary that, for each
+    guest (first key), maps the happiness they would gain or lose (key) by
+    sitting next to each of the other guests (second key).
+    """
+    input_data = {}
+    regex_happiness_line = re.compile(
         r"^([A-Z][a-z]+) would (gain|lose) (\d+) happiness units by sitting "
         r"next to ([A-Z][a-z]+).$")
-    with open("./inputs/day_13.txt") as file:
+    with open("./inputs/day_13.txt", encoding="utf-8") as file:
         for line in file.readlines():
             # Skip blank lines
             line = line.strip()
             if len(line) == 0:
                 continue
             # Extract interaction fields
-            m = regex_happ.match(line)
-            person_1 = m.group(1)
-            person_2 = m.group(4)
-            happ_units = int(m.group(3))
-            if m.group(2) == "lose":
+            match_line = regex_happiness_line.match(line)
+            person_1 = match_line.group(1)
+            person_2 = match_line.group(4)
+            happ_units = int(match_line.group(3))
+            if match_line.group(2) == "lose":
                 happ_units = -happ_units
             # Insert data into problem input - interaction is uni-directional
-            if person_1 not in input:
-                input[person_1] = {}
-            input[person_1][person_2] = happ_units
-    return input
+            if person_1 not in input_data:
+                input_data[person_1] = {}
+            input_data[person_1][person_2] = happ_units
+    return input_data
 
 
-def solve_part1(input):
+def solve_part1(input_data):
     """
-    Determines the total change in happiness for the optimal seating arrangement by checking each
-    possible order of guests, calculating result and comparing to running optimal value.
+    Determines the total change in happiness for the optimal seating arrangement
+    by checking each possible order of guests, calculating result and comparing
+    to running optimal value.
     """
-    (_max_happ_guest_order, max_happ_delta) = find_optimal_seating_arrangement(input)
-    return max_happ_delta
+    max_happiness_delta = find_maximum_happiness_delta(input_data)
+    return max_happiness_delta
 
 
-def solve_part2(input):
+def solve_part2(input_data):
     """
-    Determines the total change in happiness for the optimal seating arrangement including self
-    seated (0 happiness gained or lost for these interactions) by checking each possible order of
-    guests, calculating result and comparing to the running optimal value.
+    Determines the total change in happiness for the optimal seating arrangement
+    including self seated (0 happiness gained or lost for these interactions) by
+    checking each possible order of guests, calculating result and comparing to
+    the running optimal value.
     """
     # Update the new happiness matrix to include self
-    new_happ_matrix = dict(input)
-    old_guest_list = list(input.keys())
+    new_happiness_matrix = dict(input_data)
+    old_guest_list = list(input_data.keys())
     self_name = "Mr. Robot"
-    new_happ_matrix[self_name] = {}
+    new_happiness_matrix[self_name] = {}
     for guest_name in old_guest_list:
-        new_happ_matrix[self_name][guest_name] = 0
-        new_happ_matrix[guest_name][self_name] = 0
+        new_happiness_matrix[self_name][guest_name] = 0
+        new_happiness_matrix[guest_name][self_name] = 0
     # Find the optimal seating arrangement
-    (_max_happ_guest_order, max_happ_delta) = find_optimal_seating_arrangement(
-        new_happ_matrix)
-    return max_happ_delta
+    max_happiness_delta = find_maximum_happiness_delta(new_happiness_matrix)
+    return max_happiness_delta
 
 
-def find_optimal_seating_arrangement(happ_mat):
-    guest_list = list(happ_mat.keys())
-    guest_perms = itertools.permutations(guest_list)
+def find_maximum_happiness_delta(happiness_matrix):
+    """
+    Determines the maximum possible happiness delta in the optimal seating
+    arrangement by evaluating the happiness delta for each permutation of guests
+    and selecting the highest happiness delta observed.
+    """
+    # Determine the possible permutations of guests
+    guest_list = list(happiness_matrix.keys())
+    guest_permutations = itertools.permutations(guest_list)
     max_happiness_delta = None
-    max_happiness_guest_order = None
-    for perm in guest_perms:
+    for guest_order in guest_permutations:
+        # Calculate the happiness delta for current guest ordering
         happiness_delta = 0
-        for p1 in range(0, len(perm)):
-            # Table is circular so last guest is sitting next to first
-            p2 = p1 + 1
-            if p1 == (len(perm) - 1):
-                p2 = 0
-            happiness_delta += happ_mat[perm[p1]][perm[p2]]
-            happiness_delta += happ_mat[perm[p2]][perm[p1]]
+        for guest_1_index, guest_1_name in enumerate(guest_order):
+            guest_2_index = guest_1_index + 1
+            if guest_2_index >= len(guest_order):
+                guest_2_index = 0
+            guest_2_name = guest_order[guest_2_index]
+            happiness_delta += happiness_matrix[guest_1_name][guest_2_name]
+            happiness_delta += happiness_matrix[guest_2_name][guest_1_name]
+        # Update the maximum happiness delta value if required
         if max_happiness_delta is None:
             max_happiness_delta = happiness_delta
-            max_happiness_guest_order = list(perm)
         elif happiness_delta > max_happiness_delta:
             max_happiness_delta = happiness_delta
-            max_happiness_guest_order = list(perm)
-    return (max_happiness_guest_order, max_happiness_delta)
+    return max_happiness_delta
 
 
 if __name__ == "__main__":
