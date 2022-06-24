538d4842e261ede3fc951fb74887ae6041d09e56
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-06-24 14:57:35 +1000

    Update Day 15 code to conform with pylint style

--- solutions/day_15.py
+++ solutions/day_15.py
@@ -1,9 +1,17 @@
+"""
+Solutions for AOC 2015 Day 15.
+"""
+
+
 import re
 from dataclasses import dataclass
 
 
 @dataclass
 class Ingredient:
+    """
+    Dataclass used to represent one of the possible cookie ingredients.
+    """
     name: str
     capacity: int
     durability: int
@@ -13,51 +21,60 @@ class Ingredient:
 
 
 def main():
-    input = process_input_file()
-    p1_solution = solve_part1(input)
-    print("P1 solution - {}".format(p1_solution))
-    p2_solution = solve_part2(input)
-    print("P2 solution - {}".format(p2_solution))
+    """
+    Solves AOC 2015 Day 15 Parts 1 and 2, printing out the solutions.
+    """
+    input_data = process_input_file()
+    p1_solution = solve_part1(input_data)
+    print(f"P1 solution - {p1_solution}")
+    p2_solution = solve_part2(input_data)
+    print(f"P2 solution - {p2_solution}")
 
 
 def process_input_file():
-    input = []
+    """
+    Processes the AOC 2015 Day 15 input file into the format required by the
+    solver functions. Returned data structure is a list of the ingredient
+    objects specified in the input file.
+    """
+    input_data = {}
     regex_ingredient = re.compile(
         r"^([a-zA-Z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), "
         r"texture (-?\d+), calories (-?\d+)$")
-    with open("./inputs/day_15.txt") as file:
+    with open("./inputs/day_15.txt", encoding="utf-8") as file:
         for line in file.readlines():
             line = line.strip()
             if len(line) == 0:
                 continue
-            m = regex_ingredient.match(line)
-            name = m.group(1)
-            capacity = int(m.group(2))
-            durability = int(m.group(3))
-            flavor = int(m.group(4))
-            texture = int(m.group(5))
-            calories = int(m.group(6))
+            # Extract ingredient parameters from input file line
+            match_ingredient = regex_ingredient.match(line)
+            name = match_ingredient.group(1)
+            capacity = int(match_ingredient.group(2))
+            durability = int(match_ingredient.group(3))
+            flavor = int(match_ingredient.group(4))
+            texture = int(match_ingredient.group(5))
+            calories = int(match_ingredient.group(6))
             ingredient = Ingredient(
                 name, capacity, durability, flavor, texture, calories)
-            input.append(ingredient)
-    return input
+            input_data[name] = ingredient
+    return input_data
 
 
-def solve_part1(input):
+def solve_part1(input_data):
     """
     Finds the total score for the highest-scoring cookie by combining the input
     ingredients, using exactly 100 units (tsp) of ingredients total.
     """
-    return calculate_optimal_cookie_score(input)
+    return calculate_optimal_cookie_score(input_data)
 
 
-def solve_part2(input):
+def solve_part2(input_data):
     """
     Finds the total score for the highest-scoring cookie with exactly 500
     calories by combining the input ingredients, using exactly 100 units (tsp)
     of ingredients total.
     """
-    return calculate_optimal_cookie_score(input, calorie_req=500)
+    return calculate_optimal_cookie_score(input_data, calorie_req=500)
 
 
 def calculate_optimal_cookie_score(ingredients, calorie_req=None):
@@ -67,34 +84,41 @@ def calculate_optimal_cookie_score(ingredients, calorie_req=None):
     100 units (tsp) of ingredients total.
     """
     max_cookie_score = None
-    for n0 in range(0, 100):    # sprinkes
-        for n1 in range(0, 100 - n0):   # peanutbutter
-            for n2 in range(0, 100 - n0 - n1):  # frosting
-                n3 = 100 - n0 - n1 - n2  # sugar
+    for q_sprinkles in range(0, 100):
+        for q_peanutbutter in range(0, 100 - q_sprinkles):
+            for q_frosting in range(0, 100 - q_sprinkles - q_peanutbutter):
+                q_sugar = 100 - q_sprinkles - q_peanutbutter - q_frosting
                 # Check calorie total
                 if calorie_req is not None:
-                    calories = ingredients[0].calories * n0 + ingredients[1].calories * \
-                        n1 + ingredients[2].calories * \
-                        n2 + ingredients[3].calories * n3
+                    calories = ingredients["Sprinkles"].calories * q_sprinkles + \
+                        ingredients["PeanutButter"].calories * q_peanutbutter + \
+                        ingredients["Frosting"].calories * q_frosting + \
+                        ingredients["Sugar"].calories * q_sugar
                     if calories != calorie_req:
                         continue
-                # Calculate parameter scores
-                score_c = ingredients[0].capacity * n0 + ingredients[1].capacity * \
-                    n1 + ingredients[2].capacity * \
-                    n2 + ingredients[3].capacity * n3
-                score_d = ingredients[0].durability * n0 + ingredients[1].durability * \
-                    n1 + ingredients[2].durability * \
-                    n2 + ingredients[3].durability * n3
-                score_f = ingredients[0].flavor * n0 + ingredients[1].flavor * \
-                    n1 + ingredients[2].flavor * \
-                    n2 + ingredients[3].flavor * n3
-                score_t = ingredients[0].texture * n0 + ingredients[1].texture * \
-                    n1 + ingredients[2].texture * \
-                    n2 + ingredients[3].texture * n3
-                # Calculate cookie score
+                # Calculate cookie property scores
+                capacity = ingredients["Sprinkles"].capacity * q_sprinkles + \
+                    ingredients["PeanutButter"].capacity * q_peanutbutter + \
+                    ingredients["Frosting"].capacity * q_frosting + \
+                    ingredients["Sugar"].capacity * q_sugar
+                durability = ingredients["Sprinkles"].durability * q_sprinkles + \
+                    ingredients["PeanutButter"].durability * q_peanutbutter + \
+                    ingredients["Frosting"].durability * q_frosting + \
+                    ingredients["Sugar"].durability * q_sugar
+                flavour = ingredients["Sprinkles"].flavor * q_sprinkles + \
+                    ingredients["PeanutButter"].flavor * q_peanutbutter + \
+                    ingredients["Frosting"].flavor * q_frosting + \
+                    ingredients["Sugar"].flavor * q_sugar
+                texture = ingredients["Sprinkles"].texture * q_sprinkles + \
+                    ingredients["PeanutButter"].texture * q_peanutbutter + \
+                    ingredients["Frosting"].texture * q_frosting + \
+                    ingredients["Sugar"].texture * q_sugar
+                # Calculate cookie score and update maximum value if required
                 cookie_score = 0
-                if score_c > 0 and score_d > 0 and score_f > 0 and score_t > 0:
-                    cookie_score = score_c * score_d * score_f * score_t
+                if capacity > 0 and durability > 0 and flavour > 0 and \
+                        texture > 0:
+                    cookie_score = capacity * durability * \
+                        flavour * texture
                 if max_cookie_score is None or cookie_score > max_cookie_score:
                     max_cookie_score = cookie_score
     return max_cookie_score
