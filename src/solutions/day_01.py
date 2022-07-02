9c188a11302b32682df72fa79526f12dc48dc195
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-02 21:52:14 +1000

    Tidy up Day 1 code

--- src/solutions/day_01.py
+++ src/solutions/day_01.py
@@ -5,7 +5,9 @@ Solutions for AOC 2015 Day 1.
 
 def process_input_file():
     """
-    Processes the AOC 2015 Day 1 input file into format for solvers.
+    Processes the AOC 2015 Day 1 input file into the format required by the
+    solver functions. Returned value is the string of floor movement
+    instructions specified in the input file.
     """
     with open("./input/day_01.txt", encoding="utf-8") as file:
         return file.read().strip()
@@ -13,10 +15,10 @@ def process_input_file():
 
 def solve_part1(input_data):
     """
-    Solves AOC 2015 Day 1 Part 1 // alculates the final floor result from
+    Solves AOC 2015 Day 1 Part 1 // Calculates the final floor result from
     processing the input instructions.
     """
-    floor = 0
+    floor = 0   # Begin at ground floor
     for char in input_data:
         if char == '(':
             floor += 1
@@ -30,8 +32,8 @@ def solve_part2(input_data):
     Solves AOC 2015 Day 1 Part 2 // Calculates the location of the input
     character that causes Santa to first enter the basement floor.
     """
-    pos = 1
-    floor = 0
+    position = 1    # First character in the instructions has "position" 1
+    floor = 0       # Begin at ground floor
     for char in input_data:
         if char == '(':
             floor += 1
@@ -41,5 +43,5 @@ def solve_part2(input_data):
         if floor == -1:
             break
         # Continue to the next character
-        pos += 1
-    return pos
+        position += 1
+    return position
