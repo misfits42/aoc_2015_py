d70d830b8c967aa9f1ba2f385d2e52f0f625d394
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-07-02 21:51:55 +1000

    Tidy up Day 2 code

--- src/solutions/day_02.py
+++ src/solutions/day_02.py
@@ -8,20 +8,22 @@ import re
 
 def process_input_file():
     """
-    Processes the AOC 2015 Day 2 input file into format for solver functions.
+    Processes the AOC 2015 Day 2 input file into the format required by the
+    solver functions. Returned value is a list containing the dimensions of the
+    presents specified in the input file.
     """
     with open("./input/day_02.txt", encoding="utf-8") as file:
         raw_input = file.read().strip()
         input_data = []
         regex = re.compile(r"(\d+)x(\d+)x(\d+)")
         for line in raw_input.splitlines():
-            line = line.strip()
-            if len(line) == 0:
+            if len(line := line.strip()) == 0:
                 continue
-            match_r = regex.match(line)
-            line_data = (int(match_r.group(1)), int(match_r.group(2)),
-                         int(match_r.group(3)))
-            input_data.append(line_data)
+            match_line = regex.match(line)
+            length = int(match_line.group(1))
+            width = int(match_line.group(2))
+            height = int(match_line.group(3))
+            input_data.append((length, width, height))
         return input_data
 
 
@@ -31,8 +33,8 @@ def solve_part1(input_data):
     paper required to wrap the presents specified in input.
     """
     total_paper = 0  # unit: square feet
-    for data in input_data:
-        side_areas = [data[0] * data[1], data[0] * data[2], data[1] * data[2]]
+    for (length, width, height) in input_data:
+        side_areas = [length * width, length * height, width * height]
         slack = min(side_areas)
         total_paper += 2 * sum(side_areas) + slack
     return total_paper
@@ -44,10 +46,10 @@ def solve_part2(input_data):
     feet required to wrap the presents specified in input.
     """
     total_ribbon = 0    # unit: feet
-    for data in input_data:
-        side_perims = [2 * (data[0] + data[1]), 2 *
-                       (data[0] + data[2]), 2 * (data[1] + data[2])]
-        volume = data[0] * data[1] * data[2]
-        ribbon = min(side_perims) + volume
+    for (length, width, height) in input_data:
+        perimeters = [2 * (length + width), 2 * (length + height),
+                      2 * (width + height)]  # for each face
+        volume = length * width * height
+        ribbon = min(perimeters) + volume
         total_ribbon += ribbon
     return total_ribbon
